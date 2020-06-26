186cb46bd3815a1f3cc56fb801c2f6a059967d84
Sheila Kelley <spkelley26@gmail.com>
2020-06-26 15:24:17 -0700

    changed database

--- devskills_djangoapi/settings.py
+++ devskills_djangoapi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'herokuapp'
+    'herokuapp',
+    'tastypie'
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,8 @@ WSGI_APPLICATION = 'devskills_djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'postgresql'),
     }
 }
 
